9d2eacfb9a846b0b49bb84b643198983054c81a0
Yuqi <ywang@bayareametro.gov>
2021-09-09 10:14:54 -0700

    add notes for running it on a Windows machine

--- notebooks/pipeline/step1_shst_extraction.sh
+++ notebooks/pipeline/step1_shst_extraction.sh
@@ -14,13 +14,18 @@
 # docker image list
 #
 # Run a command (/bin/bash) in a new container layer over the specified image:
-# docker run -it --rm -v [path to this directory]:/usr/node/ shst:latest /bin/bash
-# e.g. docker run -it --rm -v /Users/lzorn/Documents/GitHub/travel-model-two-networks:/usr/node/ shst:latest /bin/bash
+# 	docker run -it --rm -v [path to this directory]:/usr/node/ shst:latest /bin/bash
+# On a Mac machine:
+# 	e.g. docker run -it --rm -v /Users/lzorn/Documents/GitHub/travel-model-two-networks:/usr/node/ shst:latest /bin/bash
+# On a Windows machine:
+#   e.g. docker run -it --rm -v /c/Users/ywang/Documents/GitHub/travel-model-two-networks:/usr/node/ shst:latest /bin/bash
 
 # Then you can cd to this directory, make this script executable, and run this script:
 # cd /usr/node/notebooks/pipeline
-# chmod u+x step1_shst_extraction.sh
-# ./step1_shst_extraction.sh
+# chmod u+x step1_shst_extraction.sh (skip this line when running on a Windows machine)
+# ./step1_shst_extraction.sh (if getting "/bin/bash^M: bad interpreter: No such file or directory" error, 
+#	it means this file has Windows line endings, remove them by running "sed -i -e 's/\r$//' step1_shst_extraction.sh" before this step)
+
 
 for i in {1..14}
 do
