959b5bde8ee97ff06f6e669765aa38d8bbf68fc0
Flavia Tsang <flavia.tsang@gmail.com>
2022-03-01 00:34:39 -0800

    add transit layers

--- notebooks/network_QA/create_aprx_for_feature_layer.py
+++ notebooks/network_QA/create_aprx_for_feature_layer.py
@@ -4,21 +4,48 @@
 # I start from manually starting a blank project 
 # and run the following code in the python window within ArcGIS Pro
 # 
-# this script is stil in development - there are some issues related to reordering the layers
 # --------------------------------------------------------------------------------
 
 import arcpy
 
-# county_list = ["Alameda", "Contra Costa", "Marin",  "Napa",  "San Francisco",  "San Mateo",  "Santa Clara",  "Solano",  "Sonoma"]
-county_list = ["Marin",  "Napa",  "San Francisco",  "San Mateo",  "Santa Clara",  "Solano",  "Sonoma"]
-#county_list = ["Marin"] 
-county_list = ["Napa"]
+# county list - note the underscores
+county_list = ["Alameda", "Contra_Costa", "Marin",  "Napa",  "San_Francisco",  "San_Mateo",  "Santa_Clara",  "Solano",  "Sonoma"]
+#county_list = ["Alameda"]
 
 for CountyName in county_list:
 
     # This script uses the keyword current - it should be run from the Python window within ArcGIS Pro 
+    # To make this a stand alone script, I can probably just specify a project in between the quotes e.g. aprx = arcpy.mp.ArcGISProject(r"C:\Projects\etc\etc.aprx"), but I haven't tested it.
     aprx = arcpy.mp.ArcGISProject("CURRENT")
     aprxMap = aprx.listMaps("Map")[0] 
+
+
+    # add transit layers
+    # ----------------------------
+    # transit links
+    lyrToAdd = r"M:\Data\GIS layers\TM2_Networks\model_net.gdb\transit_links"
+    aprxMap.addDataFromPath(lyrToAdd)
+
+    # import symbology 
+    arcpy.management.ApplySymbologyFromLayer("transit_links", r"M:\Development\Travel Model Two\Supply\Network_QA_2022\Maps_to_publish\Symbology_layers\transit_links.lyrx", None, "MAINTAIN")
+
+    # fare zones
+    lyrToAdd = r"M:\Data\GIS layers\TM2_Networks\model_net.gdb\nodes"
+    aprxMap.addDataFromPath(lyrToAdd)
+
+    # apply definition query (TANA, or non TANA)
+    l = aprxMap.listLayers("nodes")[0]
+    l.definitionQuery = "farezone <> 0"
+    # rename layer
+    for lyr in aprxMap.listLayers():
+        print(lyr.name)
+        if lyr.name=="nodes":
+           layerName = str(lyr.name)
+           lyr.name = lyr.name.replace(layerName, "nodes_farezone")
+           print(f"Layer renamed to: {lyr.name}")  
+
+    # import symbology 
+    arcpy.management.ApplySymbologyFromLayer("nodes_farezone", r"M:\Development\Travel Model Two\Supply\Network_QA_2022\Maps_to_publish\Symbology_layers\nodes_farezone.lyrx", None, "MAINTAIN")
 
     # add TM2 links layers - TANA and non TANA
     # ----------------------------
@@ -47,12 +74,6 @@ for CountyName in county_list:
 
         # import symbology
         arcpy.management.ApplySymbologyFromLayer("links_" + CountyName + " " + TANAnonTANA, r"M:\Development\Travel Model Two\Supply\Network_QA_2022\Maps_to_publish\Symbology_layers\links_" + TANAnonTANA + ".lyrx", None, "MAINTAIN")
-
-        # add the comment field
-        #inFeatures = aprxMap.listLayers("links_" + CountyName + " " + TANAnonTANA)[0]
-        #fieldName = "CTA comments"
-        #fieldLength = 500
-        #arcpy.AddField_management(inFeatures, fieldName, "TEXT", field_length=fieldLength)
 
 
     # add maz and taz layers
@@ -90,17 +111,69 @@ for CountyName in county_list:
  
     print(f"Finished re-ordering.")
 
-    # save the project
+    # to reduce clutter, turn off the layers except for the TANA layer
+
+    layer_to_turn_off = aprxMap.listLayers("links_" + CountyName +" nonTANA")[0]
+    layer_to_turn_off.visible = False
+
+    layer_to_turn_off = aprxMap.listLayers("tazs_" + CountyName)[0]
+    layer_to_turn_off.visible = False
+ 
+    layer_to_turn_off = aprxMap.listLayers("mazs_" + CountyName)[0]
+    layer_to_turn_off.visible = False
+
+    layer_to_turn_off = aprxMap.listLayers("transit_links")[0]
+    layer_to_turn_off.visible = False
+
+    layer_to_turn_off = aprxMap.listLayers("nodes_farezone")[0]
+    layer_to_turn_off.visible = False
+
+    # change the zoom
+    #layer_for_zoomExtent = aprxMap.listLayers("links_" + CountyName +" TANA")[0]
+    #lyt = aprx.listLayouts()[0]
+    #mf = lyt.listElements("mapframe_element", "*")[0]
+    #mf.camera.setExtent(mf.getLayerExtent(layer_for_zoomExtent, False, True)) # or (layer,True,True)?
+    #mf.zoomToAllLayers()
+    # zoom to layer does not work yet. May need a "layout". See reference here:
+    # https://pro.arcgis.com/en/pro-app/latest/arcpy/mapping/mapframe-class.htm
+
+    # save it as a new project
     # aprx.save()
     aprx.saveACopy(r"M:\Development\Travel Model Two\Supply\Network_QA_2022\Maps_to_publish\TM2_RoadwayQA_" + CountyName + ".aprx")
     print(f"Saved the project for " + CountyName)
 
+    # remove layers in the current project (to get ready for the next county)
+    layer_to_remove = aprxMap.listLayers("links_" + CountyName +" TANA")[0]
+    aprxMap.removeLayer(layer_to_remove)
+
+    layer_to_remove = aprxMap.listLayers("links_" + CountyName +" nonTANA")[0]
+    aprxMap.removeLayer(layer_to_remove)
+
+    layer_to_remove = aprxMap.listLayers("tazs_" + CountyName)[0]
+    aprxMap.removeLayer(layer_to_remove)
+ 
+    layer_to_remove = aprxMap.listLayers("mazs_" + CountyName)[0]
+    aprxMap.removeLayer(layer_to_remove)
+
+    layer_to_remove = aprxMap.listLayers("transit_links")[0]
+    aprxMap.removeLayer(layer_to_remove)
+
+    layer_to_remove = aprxMap.listLayers("nodes_farezone")[0]
+    aprxMap.removeLayer(layer_to_remove)
+
+    aprx.save()
+
+
 
 # final steps that are done manually
 # ----------------------------
-# add the CTA's model network layer (note that TM2 Marin is proprietary, so don't post it)
-# in the contents pane, uncheck the layers excep for the TANA layer
-# publish using the script publish_web_feature_layer.py
-# make the feature layer a web map
-# configure the sharing settings and editing settings on ArcGIS Online
+# add the CTA's model network layer (reprojecting their map seem necessary or it may not display properly on the web map even though it may display properly on the .aprx). Note that TM2 Marin is proprietary, so don't post it)
+# zoom into some place so they can see a close up view of the links when the map is loaded up.
+
+# publish the feature layer manually 
+# I started the script publish_web_feature_layer.py but it doesn't configure sharing and editing settings 
+# e.g. general: share with everyone
+# also we may want to provide "login required" version where we allow editing (feature property configuration: update feature attributes only)
+# finally, make the feature layer a web map (save as "TM2 QA – CountyName 2015"; add text to the descriptions)
+
 
