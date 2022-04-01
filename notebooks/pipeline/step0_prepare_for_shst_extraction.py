fcc1e1b516bd486a1566ce8538a542931b2e3a29
Yuqi <ywang@bayareametro.gov>
2022-03-31 22:49:06 -0700

    set up logging

--- notebooks/pipeline/step0_prepare_for_shst_extraction.py
+++ notebooks/pipeline/step0_prepare_for_shst_extraction.py
@@ -11,39 +11,46 @@ Output: 14 geojson files
 from methods import *
 import geopandas as gpd
 import os
+from pyproj import CRS
+from network_wrangler import WranglerLogger, setupLogging
+from datetime import datetime
 
 # input/output directory and files
-NETWORKS_DIR = r'M:\Development\Travel Model Two\Networks\TM2_network_v13'
-INPUT_DIR = os.path.join(NETWORKS_DIR, 'external', 'step0_boundaries')
+ROOT_DATA_DIR = 'C:\\Users\\{}\\Box\\Modeling and Surveys\\Development\\Travel Model Two Development\\Build_TM2_networkV13_pipeline'.format(os.getenv('USERNAME'))
+INPUT_DIR = os.path.join(ROOT_DATA_DIR, 'external', 'step0_boundaries')
 COUNTY_POLYGON = 'county_5m - Copy.shp'
-OUTPUT_DIR = os.path.join(NETWORKS_DIR, 'external', 'step0_boundaries', 'modified')
+OUTPUT_DIR = os.path.join(ROOT_DATA_DIR, 'external', 'step0_boundaries', 'modified')
 
 # # EPSG requirement
 # TARGET_EPSG = 4326
-
+lat_lon_epsg_str = 'epsg:{}'.format(str(LAT_LONG_EPSG))
 
 if __name__ == '__main__':
-
-    # read polygon boundry
-    county_polys_gdf = gpd.read_file(os.path.join(INPUT_DIR, COUNTY_POLYGON))
-    # "../../data/external/county_boundaries/county_5m - Copy.shp")
-
-    print('Input county boundary file uses projection: ' + str(county_polys_gdf.crs))
-
-    # project to lat-long
-    county_polys_gdf = county_polys_gdf.to_crs(epsg=LAT_LONG_EPSG)
-    print('converted to projection: ' + str(county_polys_gdf.crs))
-    # print(county_polys_gdf)
-
     # create output folder if not exist
     if not os.path.exists(OUTPUT_DIR):
-        print('create output folder')
+        WranglerLogger.info('create output folder: {}'.format(OUTPUT_DIR))
         os.makedirs(OUTPUT_DIR)
+
+    # setup logging
+    LOG_FILENAME = os.path.join(
+        OUTPUT_DIR,
+        "step0_prepare_for_shst_extraction_{}.info.log".format(datetime.now().strftime("%Y_%m_%d__%H_%M_%S")),
+    )
+    setupLogging(LOG_FILENAME)
+
+    # read polygon boundary
+    county_polys_gdf = gpd.read_file(os.path.join(INPUT_DIR, COUNTY_POLYGON))
+
+    WranglerLogger.info('Input county boundary file uses projection: ' + str(county_polys_gdf.crs))
+
+    # project to lat-long
+    county_polys_gdf = county_polys_gdf.to_crs(CRS(lat_lon_epsg_str))
+    WranglerLogger.info('converted to projection: ' + str(county_polys_gdf.crs))
 
     # export polygon to geojson for shst extraction
     i = 1
     for g in county_polys_gdf.geometry:
-        print('exporting boundary file number: ' + str(i))
+        WranglerLogger.info('exporting boundary file number: ' + str(i))
 
         boundary_gdf = gpd.GeoDataFrame({"geometry": gpd.GeoSeries(g)})
 
--- notebooks/pipeline/step2_osmnx_extraction.py
+++ notebooks/pipeline/step2_osmnx_extraction.py
@@ -2,70 +2,58 @@ USAGE = """
 
 Extracts complete OSM attributes using OSMNX.
 Input: polygon boundary file for the region
-Output: geojson files from OSMNX
+Output: nodes and links data from OSMNX in geojson format
 """
-
-import geopandas as gpd
-import json
-import networkx as nx
-import pandas as pd
-import osmnx as ox
-import os
-import time
-
-# from methods import point_df_to_geojson
-# from methods import link_df_to_geojson
-# import methods
 from methods import *
+from network_wrangler import WranglerLogger, setupLogging
+from datetime import datetime
 
 # input/output directory and files
-NETWORKS_DIR = r'M:\Development\Travel Model Two\Networks\TM2_network_v13'
-INPUT_DIR = os.path.join(NETWORKS_DIR, 'external', 'step0_boundaries')
+ROOT_DATA_DIR = 'C:\\Users\\{}\\Box\\Modeling and Surveys\\Development\\Travel Model Two Development\\Build_TM2_networkV13_pipeline'.format(os.getenv('USERNAME'))
+INPUT_DIR = os.path.join(ROOT_DATA_DIR, 'external', 'step0_boundaries')
 COUNTY_POLYGON = 'county_5m - Copy.shp'
-OUTPUT_DIR = os.path.join(NETWORKS_DIR, 'external', 'step2_osmnx_extraction')
+OUTPUT_DIR = os.path.join(ROOT_DATA_DIR, 'external', 'step2_osmnx_extraction')
 
 if __name__ == '__main__':
+    # create output folder if not exist
+    if not os.path.exists(OUTPUT_DIR):
+        WranglerLogger.info('create output folder')
+        os.makedirs(OUTPUT_DIR)
+
+    # setup logging
+    LOG_FILENAME = os.path.join(
+        OUTPUT_DIR,
+        "step2_osmnx_extraction_{}.info.log".format(datetime.now().strftime("%Y_%m_%d__%H_%M_%S")),
+    )
+    setupLogging(LOG_FILENAME)
 
     # read polygon boundary
     county_polys_gdf = gpd.read_file(os.path.join(INPUT_DIR, COUNTY_POLYGON))
-    print('Input county boundary file uses projection: ' + str(county_polys_gdf.crs))
+    WranglerLogger.info('Input county boundary file uses projection: ' + str(county_polys_gdf.crs))
 
     # project to lat-long
     county_polys_gdf = county_polys_gdf.to_crs(epsg=LAT_LONG_EPSG)
-    print('converted to projection: ' + str(county_polys_gdf.crs))
+    WranglerLogger.info('converted to projection: ' + str(county_polys_gdf.crs))
 
     # dissolve into one polygon (osmnx extraction doesn't have the area limitation as ShSt extraction)
-    print('dissolve into one polygon')
+    WranglerLogger.info('dissolve into one polygon')
     boundary = county_polys_gdf.geometry.unary_union
 
     # OSM extraction
-    print('starting osmnx extraction')
-    # starting time
-    start = time.time()
-
+    WranglerLogger.info('starting osmnx extraction')
     G_drive = ox.graph_from_polygon(boundary, network_type='all', simplify=False)
+    WranglerLogger.info('finished osmnx extraction')
 
-    # end time
-    end = time.time()
-    print('finished osmnx extraction, tool {} minutes'.format((end - start)/60))
-
-    print('getting links and nodes')
+    WranglerLogger.info('getting links and nodes from osmnx data')
     link_gdf = ox.graph_to_gdfs(G_drive, nodes=False, edges=True)
-    print('link_gdf has {} records, with columns: \n{}'.format(link_gdf.shape[0],
-                                                               list(link_gdf)))
+    WranglerLogger.info('link_gdf has {} records, with columns: \n{}'.format(
+        link_gdf.shape[0], list(link_gdf)))
     node_gdf = ox.graph_to_gdfs(G_drive, nodes=True, edges=False)
-    print('node_gdf has {} records, with columns: \n{}'.format(node_gdf.shape[0],
-                                                               list(node_gdf)))
-
-    # create output folder if not exist
-    if not os.path.exists(OUTPUT_DIR):
-        print('create output folder')
-        os.makedirs(OUTPUT_DIR)
-
-    print('writing out OSM links and nodes to geojson')
-    start = time.time()
+    WranglerLogger.info('node_gdf has {} records, with columns: \n{}'.format(
+        node_gdf.shape[0], list(node_gdf)))
 
     # writing out OSM link data to geojson
+    WranglerLogger.info('writing out OSM links and nodes to geojson at {}'.format(OUTPUT_DIR))
     link_prop = link_gdf.drop("geometry", axis=1).columns.tolist()
     link_geojson = link_df_to_geojson(link_gdf, link_prop)
     with open(os.path.join(OUTPUT_DIR, 'link.geojson'), "w") as f:
@@ -77,5 +65,4 @@ if __name__ == '__main__':
     with open(os.path.join(OUTPUT_DIR, 'node.geojson'), "w") as f:
         json.dump(node_geojson, f)
 
-    end = time.time()
-    print('finished writing out OSM links and nodes, took {} minutes'.format((end - start)/60))
+    WranglerLogger.info('finished writing out OSM links and nodes')
