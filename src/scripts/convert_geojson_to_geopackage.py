1578abc8ee395ab74efa18f36ec0a83ea2906ae4
Lisa Zorn <lzorn@bayareametro.gov>
2022-05-16 14:45:48 -0700

    Add logfile

--- src/scripts/convert_geojson_to_geopackage.py
+++ src/scripts/convert_geojson_to_geopackage.py
@@ -5,13 +5,22 @@ USAGE = """
 
 """
 
-import argparse, glob, os, sys
+import argparse, datetime, glob, os, sys
+import pandas as pd
 import geopandas
+from network_wrangler import WranglerLogger, setupLogging
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=USAGE, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('input_geojson_dir', help='Input directory for geojson files')
     parser.add_argument('output_gpkg',   help='Output GeoPackage file')
+
+    # setup logging
+    pd.set_option("display.max_rows", 500)
+    pd.set_option("display.max_columns", 500)
+    pd.set_option("display.width", 50000)
+    LOG_FILENAME = "convert_geojson_to_geopackage_{}.log".format(datetime.datetime.now().strftime("%Y%m%d_%H%M"))
+    setupLogging(LOG_FILENAME)
 
     args = parser.parse_args()
 
@@ -19,26 +28,26 @@ if __name__ == '__main__':
     file_list = glob.glob(os.path.join(args.input_geojson_dir,"*.geojson"))
     if len(file_list)==0:
         error_str = 'No .geojson files found in [{}]'.format(args.input_geojson_dir)
-        print(error_str)
+        WranglerLogger.fatal(error_str)
         sys.exit(error_str)
 
     # check output geopackage doesn't exist
     if os.path.exists(args.output_gpkg):
         error_str = 'output_gpkg [{}] exists already'.format(args.output_gpkg)
-        print(error_str)
+        WranglerLogger.fatal(error_str)
         sys.exit(error_str)
 
     # read geojson files and output them to geopackage
     for full_filename in file_list:
-        print("Reading {}".format(full_filename))
+        WranglerLogger.info("Reading {}".format(full_filename))
         input_gdf = geopandas.read_file(full_filename)
-        print("dtypes:\n{}".format(input_gdf.dtypes))
+        WranglerLogger.info("dtypes:\n{}".format(input_gdf.dtypes))
 
         # layer name is the filename without ".geojson" and "_" instead of "."
         (dir,filename) = os.path.split(full_filename)
         layer_name = filename.replace(".geojson","")
         layer_name = layer_name.replace(".","_")
         input_gdf.to_file(args.output_gpkg, layer=layer_name, driver="GPKG")
-        print("Wrote {} to {}".format(layer_name, args.output_gpkg))
+        WranglerLogger.info("Wrote {} to {}".format(layer_name, args.output_gpkg))
 
-    print("Done")
+    WranglerLogger.info("Done")
